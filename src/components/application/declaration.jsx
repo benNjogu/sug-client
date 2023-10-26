acd0f9a8ca23b691f15920d01f03b308a2cb7f4d
benNjogu <kamandemachafi@gmail.com>
2023-10-26 15:17:48 +0300

    Fixing some logs

--- src/components/application/declaration.jsx
+++ src/components/application/declaration.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
 
 const Declaration = ({ user, updateUser }) => {
   const {
