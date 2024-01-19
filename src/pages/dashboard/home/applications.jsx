9ceeb7062953600dd7cd5e2e5e30be89ff093b5b
benNjogu <kamandemachafi@gmail.com>
2024-01-19 08:12:17 +0300

    Adding view_details_page and action buttons.

--- src/pages/dashboard/home/applications.jsx
+++ src/pages/dashboard/home/applications.jsx
@@ -9,72 +9,6 @@ import ModalComponent from '../../../components/modal/modal.component';
 import DefaultLayout from '../../../components/default-layout/default-layout.component';
 import { FetchAllApplications } from '../../../redux/slices/application';
 
-const columns = [
-  {
-    title: 'S.No',
-    dataIndex: 's_no',
-  },
-  {
-    title: 'Course',
-    dataIndex: 'course_title',
-  },
-  {
-    title: 'Provider',
-    dataIndex: 'training_provider',
-  },
-  {
-    title: 'Status',
-    dataIndex: 'approved',
-    render(text, record) {
-      return {
-        props: {
-          style: {
-            color:
-              record.approved === 'Rejected'
-                ? 'red'
-                : record.approved === 'Stage_1'
-                ? '#98FB98'
-                : record.approved === 'Stage_2'
-                ? '#32CD32'
-                : record.approved === 'Approved'
-                ? '	#008000'
-                : '',
-            fontWeight: 600,
-          },
-        },
-        children: <div>{text}</div>,
-      };
-    },
-  },
-  {
-    title: 'Action',
-    dataIndex: 'id',
-    render: (id, record) =>
-      record.approved === 'Rejected' ? (
-        <div className="d-flex justify-content-around">
-          <div className="mx-2" />
-          <EyeOutlined className="mx-2" onClick={() => console.log('eye')} />
-          <EditOutlined className="mx-2" onClick={() => console.log('eye')} />
-        </div>
-      ) : record.approved === 'Pending' ? (
-        <div className="d-flex justify-content-around">
-          <EyeOutlined className="mx-2" onClick={() => console.log('eye')} />
-          <EditOutlined className="mx-2" onClick={() => console.log('eye')} />
-          <DeleteOutlined
-            className="mx-2"
-            onClick={() => console.log('r', record.approved)}
-          />
-        </div>
-      ) : (
-        <div className="d-flex justify-content-around">
-          <div className="mx-2" />
-          <div className="mx-2" />
-          <EyeOutlined className="mx-2" onClick={() => console.log('eye')} />
-        </div>
-      ),
-  },
-];
-
 let status = {
   Rejected: -1,
   Pending: 0,
@@ -102,13 +36,101 @@ const Applications = () => {
     });
   });
 
+  const columns = [
+    {
+      title: 'S.No',
+      dataIndex: 's_no',
+    },
+    {
+      title: 'Course',
+      dataIndex: 'course_title',
+    },
+    {
+      title: 'Provider',
+      dataIndex: 'training_provider',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'approved',
+      render(text, record) {
+        return {
+          props: {
+            style: {
+              color:
+                record.approved === 'Rejected'
+                  ? 'red'
+                  : record.approved === 'Stage_1'
+                  ? '#98FB98'
+                  : record.approved === 'Stage_2'
+                  ? '#32CD32'
+                  : record.approved === 'Approved'
+                  ? '	#008000'
+                  : '',
+              fontWeight: 600,
+            },
+          },
+          children: <div>{text}</div>,
+        };
+      },
+    },
+    {
+      title: 'Action',
+      dataIndex: 'id',
+      render: (id, record) =>
+        record.approved === 'Rejected' ? (
+          <div className="d-flex justify-content-around">
+            <div className="mx-2" />
+            <EyeOutlined
+              className="mx-2"
+              onClick={() => handleViewApplication(record)}
+            />
+            <EditOutlined
+              className="mx-2"
+              onClick={() => handleEditApplication(record)}
+            />
+          </div>
+        ) : record.approved === 'Pending' ? (
+          <div className="d-flex justify-content-around">
+            <EyeOutlined
+              className="mx-2"
+              onClick={() => handleViewApplication(record)}
+            />
+            <EditOutlined
+              className="mx-2"
+              onClick={() => handleEditApplication(record)}
+            />
+            <DeleteOutlined
+              className="mx-2"
+              onClick={() => handleDeleteApplication(record)}
+            />
+          </div>
+        ) : (
+          <div className="d-flex justify-content-around">
+            <div className="mx-2" />
+            <div className="mx-2" />
+            <EyeOutlined
+              className="mx-2"
+              onClick={() => handleViewApplication(record)}
+            />
+          </div>
+        ),
+    },
+  ];
+
   const handleNextClick = () => {
     navigate('/app/new-application');
   };
 
-  const handleApplicationClick = (e) => {
-    e.preventDefault();
-    navigate('/app/view-application');
+  const handleViewApplication = (record) => {
+    navigate('/app/view-application', record);
+  };
+
+  const handleEditApplication = (record) => {
+    console.log('edit application', record);
+  };
+
+  const handleDeleteApplication = (record) => {
+    console.log('delete application', record);
   };
 
   useEffect(() => {
