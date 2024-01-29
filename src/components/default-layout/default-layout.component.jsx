0a47be0f93b5b8defb5f1a7c795a8bd06cce0ed4
benNjogu <kamandemachafi@gmail.com>
2024-01-29 09:41:57 +0300

    Adding a logout control modal

--- src/components/default-layout/default-layout.component.jsx
+++ src/components/default-layout/default-layout.component.jsx
@@ -14,12 +14,13 @@ import {
   UserOutlined,
   LogoutOutlined,
 } from '@ant-design/icons';
-import { Layout, Menu, Button, theme } from 'antd';
+import { Layout, Menu, Button, theme, Modal } from 'antd';
 
 import { LogOutUser } from '../../redux/slices/auth';
 import './default-layout.styles.css';
 
 const { Header, Sider, Content } = Layout;
+
 const DefaultLayout = ({ children }) => {
   const [collapsed, setCollapsed] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -107,7 +108,7 @@ const DefaultLayout = ({ children }) => {
           key: '/auth/login',
           icon: <LogoutOutlined />,
           label: (
-            <Link style={linkStyle} onClick={handleLogout}>
+            <Link style={linkStyle} onClick={confirm}>
               Logout
             </Link>
           ),
@@ -165,7 +166,7 @@ const DefaultLayout = ({ children }) => {
           key: '/auth/login',
           icon: <LogoutOutlined />,
           label: (
-            <Link style={linkStyle} onClick={handleLogout}>
+            <Link style={linkStyle} onClick={confirm}>
               Logout
             </Link>
           ),
@@ -232,7 +233,7 @@ const DefaultLayout = ({ children }) => {
           key: '/auth/login',
           icon: <LogoutOutlined />,
           label: (
-            <Link style={linkStyle} onClick={handleLogout}>
+            <Link style={linkStyle} onClick={confirm}>
               Logout
             </Link>
           ),
@@ -299,7 +300,7 @@ const DefaultLayout = ({ children }) => {
           key: '/auth/login',
           icon: <LogoutOutlined />,
           label: (
-            <Link style={linkStyle} onClick={handleLogout}>
+            <Link style={linkStyle} onClick={confirm}>
               Logout
             </Link>
           ),
@@ -308,67 +309,81 @@ const DefaultLayout = ({ children }) => {
     }
   };
 
+  const [modal, contextHolder] = Modal.useModal();
+  const confirm = () => {
+    modal.confirm({
+      title: 'Logout',
+      icon: <LogoutOutlined />,
+      content: 'Do you really want to logout from the site?',
+      okText: 'OK',
+      cancelText: 'CANCEL',
+      onOk: handleLogout,
+    });
+  };
+
   return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div
-          onClick={handleClickLogo}
-          className="demo-logo-vertical logo__container"
-        >
-          <Link to="/admin" style={linkStyle}>
-            <h3 className="logo__text">{'NITA'}</h3>
-          </Link>
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={window.location.pathname}
-          items={getMenuItems()}
-        />
-      </Sider>
+    <>
+      {contextHolder}
       <Layout>
-        {loading && (
-          <div className="spinner">
-            <div className="spinner-border" role="status" />
-          </div>
-        )}
-
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
+        <Sider trigger={null} collapsible collapsed={collapsed}>
           <div
-            className="avatar__container d-flex align-items-center"
-            onClick={handleViewProfile}
+            onClick={handleClickLogo}
+            className="demo-logo-vertical logo__container"
           >
-            <Avatar sx={{ bgcolor: '#673ab7' }}>OP</Avatar>
+            <Link to="/admin" style={linkStyle}>
+              <h3 className="logo__text">{'NITA'}</h3>
+            </Link>
           </div>
-        </Header>
-        <Content
-          style={{
-            margin: '10px 0px',
-            padding: 24,
-            minHeight: '80vh',
-            background: colorBgContainer,
-          }}
-        >
-          {children}
-        </Content>
-      </Layout>
-    </Layout>
-  );
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={window.location.pathname}
+            items={getMenuItems()}
+          />
+        </Sider>
+        <Layout>
+          {loading && (
+            <div className="spinner">
+              <div className="spinner-border" role="status" />
+            </div>
+          )}
 
+          <Header
+            style={{
+              padding: 0,
+              background: colorBgContainer,
+            }}
+          >
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: '16px',
+                width: 64,
+                height: 64,
+              }}
+            />
+            <div
+              className="avatar__container d-flex align-items-center"
+              onClick={handleViewProfile}
+            >
+              <Avatar sx={{ bgcolor: '#673ab7' }}>OP</Avatar>
+            </div>
+          </Header>
+          <Content
+            style={{
+              margin: '10px 0px',
+              padding: 24,
+              minHeight: '80vh',
+              background: colorBgContainer,
+            }}
+          >
+            {children}
+          </Content>
+        </Layout>
+      </Layout>
+    </>
+  );
 };
 export default DefaultLayout;
