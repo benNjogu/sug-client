dfea8dc373fdfdba83705d6d5aaf8f16ea2c942f
benNjogu <kamandemachafi@gmail.com>
2024-04-02 15:02:30 +0300

    Adding a proper error page.

--- src/pages/page-404.jsx
+++ src/pages/page-404.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import broken from "../assets/images/broken.svg";
+import "./page-404.css";
 
 const Page404 = () => {
-  return <>404</>;
+  return (
+    <>
+      <div className="errorImageOverlay">
+        <img
+          className="errorImageContainer"
+          // src={require("../assets/images/broken.png")}
+          src={broken}
+          alt="a picture of broken vase"
+        />
+        <div className="errorImageText">Sorry this page is broken</div>
+      </div>
+    </>
+  );
 };
-
 
 export default Page404;
