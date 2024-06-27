04f121764679f45b2a039c45648375d90e9b48da
benNjogu <kamandemachafi@gmail.com>
2024-06-27 19:24:14 +0300

    Fixed admin profile

--- src/pages/dashboard-admin/admin-profile.jsx
+++ src/pages/dashboard-admin/admin-profile.jsx
@@ -16,7 +16,7 @@ const AdminProfile = () => {
   const [loading, setLoading] = useState(false);
 
   const { user_data } = useSelector((state) => state.auth);
-  let { user_name, email, account_type, created_on } = user_data;
+  let { user_name, email, account_type, created_on, active } = user_data;
 
   created_on = convertDigitInString(created_on.split("T")[0]);
 
@@ -27,8 +27,9 @@ const AdminProfile = () => {
     handleSubmit,
     formState: { errors },
   } = useForm({
-    defaultValues:
-      admin_profile_data === null ? {} : { phone: admin_profile_data[0].phone },
+    defaultValues: {
+      phone: admin_profile_data[0]?.phone,
+    },
   });
 
   const onSubmit = (data) => {
@@ -50,98 +51,105 @@ const AdminProfile = () => {
     <DefaultLayout>
       <Form className="" onSubmit={handleSubmit(onSubmit)}>
         <Spinner loading={loading} />
-
-        <div class="form-row">
-          <legend className="text-info">Profile Details.</legend>
-          <div class="col-md-4 form-group">
-            <label for="name">Name:</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              class="form-control"
-              placeholder="Bernad Njogu"
-              value={user_name}
-              readOnly
-            />
+        <div className="row">
+          <div className="col-md-12">
+            <legend className="text-info">Profile Details.</legend>
+          </div>
+          <div className="col-md-6">
+            <div class="form-row">
+              <div class="col-md-12 form-group">
+                <label for="name">Name:</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="form-control text-primary"
+                  value={user_name}
+                  readOnly
+                />
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col-md-12 form-group">
+                <label for="email">Email:</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="form-control text-primary"
+                  value={email}
+                  readOnly
+                />
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col-md-12 form-group">
+                <label for="phone">Phone Number:</label>
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  class="form-control"
+                  placeholder="Enter phone number"
+                  {...register("phone", {
+                    required: "Phone is required.",
+                  })}
+                  className={`${
+                    errors.phone
+                      ? "input-error form-control text-primary"
+                      : "form-control text-primary"
+                  }`}
+                />
+                {errors.phone && (
+                  <p className="errorMsg">{errors.phone.message}</p>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div class="form-row">
+              <div class="col-md-12 form-group">
+                <label for="level">Level:</label>
+                <input
+                  type="level"
+                  name="level"
+                  id="level"
+                  className="form-control text-primary"
+                  value={account_type}
+                  readOnly
+                />
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col-md-12 form-group">
+                <label for="active">Active:</label>
+                <input
+                  type="active"
+                  name="active"
+                  id="active"
+                  className="form-control text-primary"
+                  value={active === 1 ? "True" : "False"}
+                  readOnly
+                />
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col-md-12 form-group">
+                <label for="created">Created:</label>
+                <input
+                  type="created"
+                  name="created"
+                  id="created"
+                  className="form-control text-primary"
+                  value={created_on}
+                  readOnly
+                />
+              </div>
+            </div>
           </div>
         </div>
         <div class="form-row">
-          <div class="col-md-4 form-group">
-            <label for="email">Email:</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              class="form-control"
-              placeholder="admin@domain.com"
-              value={email}
-              readOnly
-            />
-          </div>
-        </div>
-        <div class="form-row">
-          <div class="col-md-4 form-group">
-            <label for="phone">Phone Number:</label>
-            <input
-              type="text"
-              name="phone"
-              id="phone"
-              class="form-control"
-              placeholder="Enter phone number"
-              {...register("phone", {
-                required: "Phone is required.",
-              })}
-              className={`${
-                errors.phone ? "input-error form-control" : "form-control"
-              }`}
-            />
-            {errors.phone && <p className="errorMsg">{errors.phone.message}</p>}
-          </div>
-        </div>
-        <div class="form-row">
-          <div class="col-md-4 form-group">
-            <label for="level">Level:</label>
-            <input
-              type="level"
-              name="level"
-              id="level"
-              class="form-control"
-              placeholder="Admin I"
-              value={account_type}
-              readOnly
-            />
-          </div>
-        </div>
-        <div class="form-row">
-          <div class="col-md-4 form-group">
-            <label for="active">Active:</label>
-            <input
-              type="active"
-              name="active"
-              id="active"
-              class="form-control"
-              placeholder="True"
-              readOnly
-            />
-          </div>
-        </div>
-        <div class="form-row">
-          <div class="col-md-4 form-group">
-            <label for="created">Created:</label>
-            <input
-              type="created"
-              name="created"
-              id="created"
-              class="form-control"
-              placeholder="Today"
-              value={created_on}
-              readOnly
-            />
-          </div>
-        </div>
-        <div class="form-row">
-          <div class="col-md-4 form-group">
+          <div class="col-md-12 form-group">
             <button
               type="submit"
               class="btn btn-outline-primary btn-lg d-block ml-auto"
